ce12b7dc9511753577be8faaebf0cba355a79842
Carlos San Bartolomé <carlos_a_sbs@hotmail.com>
2019-06-29 15:38:50 -0500

    Validacion commit 1

---
+++ HotelBahia.BussinesLogic/Domain/Enums/ValidacionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBahia.BussinesLogic.Domain.Enums
+{
+    public enum ValidacionType
+    {
+        error,
+        success,
+        warning,
+        info
+    }
+}
---
+++ HotelBahia.Presentacion.Web/Controllers/ValidacionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HotelBahia.BussinesLogic.Domain.Enums;
+
+namespace HotelBahia.Presentacion.Web.Controllers
+{
+    public abstract class ValidacionController : Controller
+    {
+        public void Alert(string message, ValidacionType validacionType)
+        {
+            var msg = "swal('" + validacionType.ToString().ToUpper() + "', '" + message + "','" + validacionType + "')" + "";
+            TempData["validacion"] = msg;
+        }
+    }
+}
