43e0b4295d6d5830d83b1b277d8a02e714ebfdca
Carlos San Bartolomé <carlos_a_sbs@hotmail.com>
2019-07-10 23:51:10 -0500

    cadena conexion produccion

--- HotelBahia.BussinesLogic/Servicios/AppServices/NotificacionService.cs
+++ HotelBahia.BussinesLogic/Servicios/AppServices/NotificacionService.cs
@@ -11,7 +11,7 @@ namespace HotelBahia.BussinesLogic.Servicios.AppServices
 {
     public class NotificacionService
     {
-        string Host = "https://sistemahoteleriabahia.azurewebsites.net";
+        string Host = "https://sistemahoteleria.azurewebsites.net";
         public NotificacionService()
         {
             
